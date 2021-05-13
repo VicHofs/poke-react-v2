cb2290e573667b6c69c17fd6d6eda1dd8b8a8129
Victor Hofstetter <victor.hofstetter@hi.com.br>
2021-05-13 15:14:36 -0300

    create suggestion system foundation

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -14,6 +14,7 @@ interface Props {
 const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
   const [typedSearch, setTypedSearch] = useState('');
   const [lastTypedSearch, setLastTypedSearch] = useState('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
   const [data, setData] = useState<CardProps>();
   const [greeting, setGreeting] = useState(true);
   let debounceTime: any;
@@ -27,6 +28,8 @@ const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
     'I choose...',
   ];
 
+  const blacklist = ['giratina', '487', 'deoxys', '386'];
+
   const debounceTyping = (typedWord: string) => {
     clearTimeout(debounceTime);
     debounceTime = setTimeout(() => {
@@ -36,6 +39,7 @@ const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
 
   useEffect(() => {
     const getData = async () => {
+      setSuggestions([]);
       if (typedSearch !== '') {
         setData({
           number: -666,
@@ -45,26 +49,73 @@ const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
           flavorData: [],
         });
         setGreeting(false);
+        if (typedSearch.toLowerCase().includes('nidoran')) {
+          setSuggestions(['Nidoran-m (#32)', 'Nidoran-f (#29)']);
+        }
         try {
           isFetching(true);
-          await api
-            .get<IPokemon>(`/pokemon/${typedSearch.toLowerCase()}`)
-            .then(async res => {
-              if (res.data.name !== '') {
-                await api
-                  .get<IPokemonSpecies>(`/pokemon-species/${res.data.name}`)
-                  .then(result => {
-                    setData({
-                      number: res.data.id,
-                      name: res.data.name,
-                      types: res.data.types.map(type => type.type.name),
-                      sprite: res.data.sprites.front_default,
-                      flavorData: result.data.flavor_text_entries,
+          if (blacklist.includes(typedSearch.toLowerCase())) {
+            let pokeSearch = typedSearch.toLowerCase();
+            let pokeSpeciesSearch = typedSearch.toLowerCase();
+            if (
+              typedSearch.toLowerCase() === 'giratina' ||
+              typedSearch === '487'
+            ) {
+              pokeSearch = 'giratina-altered';
+              pokeSpeciesSearch = 'giratina';
+            }
+            if (
+              typedSearch.toLowerCase() === 'deoxys' ||
+              typedSearch === '386'
+            ) {
+              pokeSearch = 'deoxys-normal';
+              pokeSpeciesSearch = 'deoxys';
+            }
+            await api
+              .get<IPokemon>(`/pokemon/${pokeSearch}`)
+              .then(async res => {
+                if (res.data.name !== '') {
+                  await api
+                    .get<IPokemonSpecies>(
+                      `/pokemon-species/${pokeSpeciesSearch}`,
+                    )
+                    .then(result => {
+                      setData({
+                        number: res.data.id,
+                        name: res.data.name.split(/\W/)[0],
+                        types: res.data.types.map(type => type.type.name),
+                        sprite: res.data.sprites.front_default,
+                        flavorData: result.data.flavor_text_entries,
+                      });
+                      isFetching(false);
                     });
-                    isFetching(false);
-                  });
-              }
-            });
+                }
+              });
+          } else {
+            await api
+              .get<IPokemon>(`/pokemon/${typedSearch.toLowerCase()}`)
+              .then(async res => {
+                if (res.data.name !== '') {
+                  await api
+                    .get<IPokemonSpecies>(`/pokemon-species/${res.data.name}`)
+                    .then(result => {
+                      setData({
+                        number: res.data.id,
+                        name: res.data.name.toLowerCase().includes('nidoran')
+                          ? res.data.name
+                              .toLowerCase()
+                              .replace('-f', '♀')
+                              .replace('-m', '♂')
+                          : res.data.name,
+                        types: res.data.types.map(type => type.type.name),
+                        sprite: res.data.sprites.front_default,
+                        flavorData: result.data.flavor_text_entries,
+                      });
+                      isFetching(false);
+                    });
+                }
+              });
+          }
         } catch (e) {
           console.error(e);
           isFetching(false);
@@ -89,6 +140,16 @@ const SearchBar: React.FC<Props> = ({ onChange, isFetching }: Props) => {
         />
         <AiOutlineSearch className="searchIcon" />
       </div>
+      {suggestions && (
+        <p>
+          did you mean&nbsp;
+          {suggestions.map(item => (
+            <button type="button" onClick={() => setTypedSearch(`${item}`)}>
+              {item}
+            </button>
+          ))}
+        </p>
+      )}
       {data && onChange(data, typedSearch) && isFetching(false)}
     </Container>
   );
